1ae6285b41b445beaa4e98b524e8fbd725a7aa92
Devraj-jha <devrajjha100806@gmail.com>
2025-08-17 15:04:23 +0530

    components

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Hello from "./Concepts/1_components"
 
 function App() {
   return (
     <div>
       <h1>Hello i AM LEARNING</h1>
       <p>we are going ot leran </p>
+      <Hello />
     </div>
   )
 }
---
+++ react-learning/src/Concepts/1_components.jsx
@@ -0,0 +1,8 @@
+//a component is just a js function that returns jsx (ui)
+// jsx = html like syntax inside js
+
+function Hello(){ //musts return jsx
+    return <h1>Hello world</h1>
+}
+
+export default Hello 
